a518aa96674679415e6cf507305ea535b309a5c7
Kassanu <kassanu@gmail.com>
2022-04-03 18:50:19 -0400

    help upgrades

---
+++ Assets/Scripts/Help.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Help : MonoBehaviour
+{
+    public int page = 0;
+
+    public GameObject[] pages;
+
+    public GameObject helpContainer;
+
+    public void openHelp() {
+        this.helpContainer.SetActive(true);
+        this.setPageActive();
+    }
+    public void closeHelp() {
+        this.helpContainer.SetActive(false);
+    }
+
+    public void nextPage() {
+        if (!((this.page + 1) >= this.pages.Length)) {
+            this.page += 1;
+            this.setPageActive();
+        }
+    }
+
+    public void previousPage() {
+        if (!((this.page - 1) < 0)) {
+            this.page -= 1;
+            this.setPageActive();
+        }
+    }
+
+    private void setPageActive() {
+        for (int i = 0; i < this.pages.Length; i++)
+        {
+            this.pages[i].SetActive(this.page == i);
+        }
+    }
+}
